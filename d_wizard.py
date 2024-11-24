dd165a625bf0f553b231e4f279b39d12076915a6
UrbanAmbrozic <urban.ambrozic@gmail.com>
2024-11-24 11:31:39 +0100

    C: Sprememba pristopa - univerzalnost - kocke definirane v global seznamu, randomized rezultat vsake kocke, zbrano v seznam

--- d_wizard.py
+++ d_wizard.py
@@ -1,19 +1,56 @@
 import random
 
-kocka_d4 = random.randint(1, 4)
-kocka_d6 = random.randint(1, 6)
-kocka_d8 = random.randint(1, 8)
-kocka_d10 = random.randint(1, 10)
-kocka_d12 = random.randint(1, 8)
-kocka_d20 = random.randint(1, 8)
+kocke = [6, 10, 20]
+kocke_strani = []
+kocke_met = []
+    
 
-print("------------------")
+def strani():
+    for x in kocke:
+        kocke_strani.append(list(range(1,x+1)))
+
+def met_kocke():
+    rezultat_meta = 0
+    for seznam_strani in kocke_strani:
+        kocke_met.append(random.choice(seznam_strani))
+            
+
+
+
+def main():
+    
+    strani()
+    met_kocke()
+    print(kocke_met)
+
+
+main()
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+kocke_strani.append
+
+
+print("--------------------")
 print("NEW DICE THROW")
-print("------------------")
+print("--------------------")
 print(f"The D4 landed on {kocka_d4}")
 print(f"The D6 landed on {kocka_d6}")
 print(f"The D8 landed on {kocka_d8}")
 print(f"The D10 landed on {kocka_d10}")
 print(f"The D12 landed on {kocka_d12}")
 print(f"The D20 landed on {kocka_d20}")
-print("------------------")
+print("--------------------")
+
+
+'''
