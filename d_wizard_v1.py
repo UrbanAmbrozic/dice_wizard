e6992f90f0b5a80f2ba3c99ade69b809b842d658
UrbanAmbrozic <urban.ambrozic@gmail.com>
2024-11-24 20:31:53 +0100

    G: Dodajanje handlinga začetnega vnosa (while-loop), dodajanje komentarjev, kopija počiščene kode (v1)

--- d_wizard_v1.py
+++ d_wizard_v1.py
@@ -1,37 +1,28 @@
-# Import modula 'random' za možnost vključevanja faktorja "naključnosti"
 import random
 
-# Funkcija za generiranje vseh vrednosti, ki jih najdemo na n-kocki.
-# Funkcija sprejme vrednost s seznama 'kocke' ( main() ) in za vsako kocko vrne seznam z njenimi vrednostmi 
 def strani(kocke):
     kocke_strani = []
     for x in kocke:
         kocke_strani.append(list(range(1, x + 1)))
     return kocke_strani
 
-# Funkcija prejme seznam z vsemi vrednostmi vseh kock (seznam seznamov).
-# Funkcija lista po posameznem seznamu iz prejete zaloge in najključno izbere eno izmed vrednosti s seznam, ki ga lista.
 def met_kocke(kocke_strani):
     kocke_meti = []
     for seznam_strani in kocke_strani:
         kocke_meti.append(random.choice(seznam_strani))
     return kocke_meti
 
-# Definirana glavna funkcija, ki prejme seznam željenih kock preko vnosa uporabnika, in preko definiranih funkcij vrne generiran naključni met za vse vnešene kocke.
 def main():
-    
-    #'While-loop' za osnovno preverjanje ustreznosti vnosa
     while True:
         uporabnik_vnos = input("Opiši mi, koliko strani imajo tvoje kocke, in pričaral ti bom naključen met teh kock (Primer: Vnesi '4' za kocko D4. Vnesi več števil za več kock): ")
         try:
-            kocke = [int(x) for x in uporabnik_vnos.split()] # Before-change: Kocke so bile vnešene ročno, kot seznam (npr. [4, 6, 20])
+            kocke = [int(x) for x in uporabnik_vnos.split()]
             break
         except ValueError:
             print("Vnašaj samo številke, ločene s presledki....prosim. Torej: ")
 
     kocke_strani = strani(kocke)
 
-    # Dodajanje 'while-loop'-a, ki bo preko definiranih funkcij povzel rezultat meta
     while True:
         kocke_meti = met_kocke(kocke_strani)
         
@@ -44,26 +35,20 @@ def main():
         
         print("---------------------------------------")     
 
-        # Nov indentiran 'while-loop', ki preverja uporabnikovo željo po ponovnem metu
         while True:
-            uporabnik_odgovor = input("Ali želiš, da ponovim met? (da/ne): ").strip().lower()
-            
-            # Uporabnikov odgovor preverjamo po skupini 'negativno' in 'pozitivno'. Če ga ne najdemo, se loop ponovi
             response_neg = ['ne', 'no', 'n', 'nej', 'nje', 'nopanjeno']
             response_pos = ['da', 'yes', 'y', 'd', 'ye', 'dej', 'vrži']
 
+            uporabnik_odgovor = input("Ali želiš, da ponovim met? (da/ne): ").strip().lower()
+
             if uporabnik_odgovor in response_neg:
                 print("Veliko sreče! Vedno sem ti na voljo, ko ti zmanjka naključnosti.")
-                exit()  # Negativen odgovor = končanje programa
-            
+                exit() 
             elif uporabnik_odgovor in response_pos:
                 print("Ponavljam met:")
-                break  # Pozitiven odgovor = izhod iz notranjega 'while-loop'-a, ki vodi v zunanji 'while-loop' - ponoven met 
-
+                break
             else:
                 print("Nisem te razumel. Prosim odgovori z da ali ne.")
 
-
-# Zagon glavne funkcije
 if __name__ == "__main__":
     main()
